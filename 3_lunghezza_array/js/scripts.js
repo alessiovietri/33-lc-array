a108e1f46517bb9222446954ad113a5a9629a0a0
Alessio Vietri <info@alessiovietri.it>
2022-12-22 12:33:11 +0100

    Aggiornato LC 3

--- 3_lunghezza_array/js/scripts.js
+++ 3_lunghezza_array/js/scripts.js
@@ -1,8 +1,11 @@
 // creazione dell'array iniziale
 //                  0       1         2
-const iscritti = ["Luca", "Marco", "Paolo"];
+const iscritti = ["Luca", "MarcoAAAA", "Paolo"];
 console.log('iscritti', iscritti, typeof iscritti);
 console.log('Lunghezza array iscritti PRE', iscritti.length);
 
 iscritti.push('Sara', 'Gianni');
 console.log('Lunghezza array iscritti POST', iscritti.length);
+
+console.log('iscritti', iscritti, typeof iscritti);
+console.log('Lunghezza elemento in array', iscritti[3], iscritti[3].length);
